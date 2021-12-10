b9333e1b74fa94af8f33704f0ee9a4beb1113d8f
Pavel Mezencev <pamezencev@gmail.com>
2021-12-10 10:18:43 +0300

    finished movie app

--- 17_movie_app/script.js
+++ 17_movie_app/script.js
@@ -6,6 +6,7 @@ const SEARCH_API =
 
 const form = document.getElementById('form');
 const search = document.getElementById('search');
+const main = document.getElementById('main');
 
 getMovies(API_URL);
 
@@ -13,7 +14,48 @@ async function getMovies(url) {
   const res = await fetch(url);
   const data = await res.json();
 
+  showMovies(data.results);
   console.log(data.results);
+}
+
+function showMovies(movies) {
+  main.innerHTML = '';
+
+  movies.forEach((movie) => {
+    const { poster_path, title, vote_average, overview } = movie;
+
+    const html = `
+    <div class="movie">
+        <img
+        src="${IMG_PATH + poster_path}"
+        alt="movie"
+        />
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getRatingColor(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+            <h3>Overview</h3>
+            <p>${overview}</p>
+        </div>
+    </div>`;
+
+    // const html = `<div class="movie">
+    //     <img src="${IMG_PATH + poster_path}">
+    // </div>`;
+
+    main.insertAdjacentHTML('beforeend', html);
+  });
+}
+
+function getRatingColor(vote) {
+  if (vote >= 8) {
+    return 'green';
+  } else if (vote >= 5) {
+    return 'orange';
+  } else {
+    return 'red';
+  }
 }
 
 form.addEventListener('submit', (e) => {
